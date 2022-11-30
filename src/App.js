9e12e50e2d2ff2b954708cf0028949a27e69af08
nazmieievilya <ilyanazmeev111@gmail.com>
2022-11-30 10:50:54 +0300

    added ability to add/edit/lock list items

--- src/App.js
+++ src/App.js
@@ -7,11 +7,20 @@ import List from "./components/List.jsx";
 function App() {
   const [list, setList] = useState([]);
   //  { id: 1, name: "Синий", isMutable: true },
+  function editId(id) {
+    const itemToEdit = list.map((item) => item.id === id);
+    console.log(itemToEdit);
+  }
   function handleAddItem() {
-    setList([
-      { id: String(Date.now()).slice(10, 13), name: "xxxx", isMutable: true },
-      ...list,
-    ]);
+    if (list.some((item) => item.productId === "")) return;
+    const newItem = {
+      id: String(Date.now()).slice(10, 13),
+      productId: "",
+      name: "xxxx",
+      isMutable: true,
+    };
+    const itemId = newItem.id;
+    setList([newItem, ...list]);
   }
   function handleRemoveAll() {
     setList([]);
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,6 +4,64 @@ import logo from "../content/KyivS.png";
 import { useRef } from "react";
 
 const ListItem = ({ itemData, setList, list }) => {
+  const productName = useRef();
+  const id = itemData.id;
+  const currentItem = list.map((item) => {
+    if (item.id === id) return item;
+  })[0];
+  function isAllowed() {
+    return list.map((item) => {
+      if (item.id === id && item.isMutable) return true;
+      return false;
+    })[0];
+  }
+
+  function toggleMutability() {
+    setList(
+      list.map((item) => {
+        if (item.id === id) {
+          item.isMutable = !item.isMutable;
+          return item;
+        }
+        return item;
+      })
+    );
+
+    console.log(isAllowed());
+  }
+  function changingId(e) {
+    if (!isAllowed()) e.target.value = currentItem.productId;
+  }
+  function changingName(e) {
+    if (!isAllowed()) e.target.value = currentItem.name;
+  }
+  function submitId(e) {
+    if (!isAllowed()) return;
+    if (e.key !== "Enter") return;
+    const newList = list.map((item) => {
+      if (item.id === id) {
+        item.productId = e.target.value;
+        return item;
+      }
+      return item;
+    });
+    setList(newList);
+    e.target.blur();
+    productName.current.focus();
+  }
+  function submitName(e) {
+    if (e.key !== "Enter") return;
+    const newList = list.map((item) => {
+      if (item.id === id) {
+        item.name = e.target.value;
+        return item;
+      }
+      return item;
+    });
+    setList(newList);
+    console.log(list);
+    e.target.blur();
+  }
   function changeName(e) {
     const id = e.target.closest(".list-item").dataset.id;
     console.log(id);
@@ -15,6 +73,7 @@ const ListItem = ({ itemData, setList, list }) => {
   return (
     <li data-id={itemData.id} className="list-item">
       <div
+        onClick={toggleMutability}
         className={
           itemData.isMutable
             ? "toggle-button-container"
@@ -24,13 +83,20 @@ const ListItem = ({ itemData, setList, list }) => {
         <div className="toggle-button"></div>
       </div>
       <p className="product">XXX-</p>
-      <p className="id">{itemData.id}</p>
+      <input
+        className="id"
+        onKeyPress={submitId}
+        onChange={changingId}
+        autoFocus
+        maxLength="3"
+      />
       <div className="product-name">
         <img className="list-img" src={logo} alt="logo" />
         <input
-          value={itemData.name}
-          onChange={changeName}
+          ref={productName}
           className="input-product-feild"
+          onKeyPress={submitName}
+          onChange={changingName}
           type="text"
         />
         <button onClick={handleRemoveItem} className="btn-remove-item">
